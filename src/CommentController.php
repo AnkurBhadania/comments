77667baea67045fd88da70c2a53e7bcdca10f4e0
Ankur Bhadania <49861097+AnkurBhadania@users.noreply.github.com>
2025-08-08 16:16:30 +0530

    Update CommentController.php
    Laravel 10.x Compatibility

--- src/CommentController.php
+++ src/CommentController.php
@@ -2,19 +2,48 @@
 
 namespace Laravelista\Comments;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Config;
 use Spatie\Honeypot\ProtectAgainstSpam;
+use Laravelista\Comments\Comment;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
 
-abstract class CommentController extends Controller implements CommentControllerInterface
+class CommentController extends Controller implements CommentControllerInterface
 {
     public function __construct()
     {
+        $this->middleware('web');
+        
         if (Config::get('comments.guest_commenting') == true) {
             $this->middleware('auth')->except('store');
             $this->middleware(ProtectAgainstSpam::class)->only('store');
         } else {
             $this->middleware('auth');
         }
+    }
+    public function store(Request $request)
+    {
+        $comment =  app(CommentService::class)->store($request);
+        return Redirect::to(URL::previous() . '#comment-' . $comment->getKey());
+    }
+
+    public function update(Request $request, Comment $comment)
+    {
+        $comment =  app(CommentService::class)->update($request, $comment);
+        return Redirect::to(URL::previous() . '#comment-' . $comment->getKey());
+    }
+
+    public function destroy(Comment $comment)
+    {
+        app(CommentService::class)->destroy($comment);
+        return Redirect::back();
+    }
+
+    public function reply(Request $request, Comment $comment)
+    {
+        $reply =  app(CommentService::class)->reply($request, $comment);
+         return Redirect::to(URL::previous() . '#comment-' . $reply->getKey());
     }
 }
